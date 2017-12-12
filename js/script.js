068a0168dcadf626ec31e47e2cccb032c63a71f3
smuniz18 <steven.muniz@gmail.com>
2017-12-12 11:02:00 -0600

    Added some media queries, and a mobile navigation system.

--- js/script.js
+++ js/script.js
@@ -38,5 +38,21 @@ $(document).ready(function() {
     }
   });
 });
-    
+
+    /* Mobile navigation */
+    $('.js--nav-icon').click(function() {
+        var nav = $('.js--main-nav');
+        var icon = $('.js--nav-icon i');
+        
+        nav.slideToggle(200);
+        if (icon.hasClass('ion-navicon-round')) {
+            icon.addClass('ion-close-round');
+            icon.removeClass('ion-navicon-round');
+        } else {
+            icon.addClass('ion-navicon-round');
+            icon.removeClass('ion-round-round');
+            
+        }        
+        ion-close-round
+    });
 });
